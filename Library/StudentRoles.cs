3038a02428d134f4e92c78d5fc95e55246483eac
haider ali khan <haiderali786.haris@gmail.com>
2025-07-19 20:48:25 +0500

    Student Roles Added

--- Library/StudentRoles.cs
+++ Library/StudentRoles.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class StudentRoles
+    public class StudentRoles
     {
+        public int Id;
+        AdminRoles adminRoles = new AdminRoles();
+        public void ViewAvailableBooks(List<Books> bookList)
+        {
+            Console.WriteLine("Available Books:");
+            foreach (var book in bookList)
+            {
+                if (book.AvailableCopies > 0)
+                {
+                    Console.WriteLine($"ID: {book.Id}, Title: {book.Title}, Author: {book.Author}, ISBN: {book.ISBN}, Genre: {book.Genre}, Available Copies: {book.AvailableCopies}");
+                }
+            }
+        }
+
+        public void BorrowABook()
+        {
+            Console.WriteLine("Tell admin the following details: ");
+            adminRoles.IssueBook();
+        }
+
+        public void ReturnBorrowedBook(List<Books> issuedBooksList, List<Books> bookList)
+        {
+            Console.WriteLine("Enter the ID of the book: ");
+            Id= Convert.ToInt32(Console.ReadLine());
+            foreach (Books book in issuedBooksList)
+            {
+                if(book.Id==Id)
+                {
+                    issuedBooksList.Remove(book);
+                    bookList.Add(book);
+                }
+            }
+            Console.WriteLine($"Book with ID {Id} returned successfully.");
+        }
     }
 }
